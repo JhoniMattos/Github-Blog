0bd1e153b55ed42bab9dc87a6e06da5c559ca654
jhoni <jhoni.psy@hotmail.com>
2025-06-11 15:25:55 +0100

    add Router

---
+++ src/Routes/Router.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Post } from "../pages/Post";
+import { Home } from "../pages/Home";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="post" element={<Post />} />
+    </Routes>
+  );
+}
