88219682115270be4ade425327ec8a94d9cddba4
jhoni <jhoni.psy@hotmail.com>
2025-06-11 15:32:51 +0100

    add Default Layouts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import background from "../../assets/background-logo.svg";
-import logo from "../../assets/logo.svg";
 
 export function Header() {
   return (
     <div>
       <img src={background} />
-      <img src={logo} />
     </div>
   );
 }
