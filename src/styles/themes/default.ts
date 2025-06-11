66213e2cc329a2c156b429dc60f5cd367b098958
jhoni <jhoni.psy@hotmail.com>
2025-06-11 14:46:23 +0100

    firts commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const DefaultTheme = {
+  "white-100": "#E7EDF4" /* title */,
+
+  "gray-100": "#C4D4E3" /* subtitle */,
+  "gray-200": "#AFC2D4" /* text */,
+
+  "gray-300": "#7B96B2" /* span */,
+  "gray-400": "#3A536B" /* label */,
+  "gray-500": "#1C2F41" /* border */,
+  "gray-600": "#112131" /* post */,
+
+  "gray-700": "#0B1B2B" /* profile */,
+  "gray-800": "#071422" /* background */,
+  "gray-900": "#040F1A" /* input */,
+
+  blue: "#3294F8",
+};
